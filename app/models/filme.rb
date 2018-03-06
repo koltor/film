9b1afa8b8dd9fb41d4c11acbf2cad7607d13a08a
koltor <maxence.theme@gmail.com>
2018-03-06 23:39:26 +0100

    ajout des bouton previous et nex

--- app/models/filme.rb
+++ app/models/filme.rb
@@ -2,4 +2,15 @@ class Filme < ApplicationRecord
 	mount_uploader :image, ImageUploader
 	mount_uploader :background, BackgroundUploader
 	mount_uploader :video, VideoUploader
+
+	scope :next, lambda {|id| where("id > ?",id).order("id ASC") }
+	scope :prev, lambda {|id| where("id < ?",id).order("id DESC") }
+
+    def next
+      Filme.next(self.id).first
+    end
+
+    def prev
+      Filme.prev(self.id).first
+    end
 end
--- app/models/manga.rb
+++ app/models/manga.rb
@@ -3,4 +3,15 @@ class Manga < ApplicationRecord
 	mount_uploader :background, BackgroundUploader;
 
 	has_many :saisons;
+
+	scope :next, lambda {|id| where("id > ?",id).order("id ASC") }
+	scope :prev, lambda {|id| where("id < ?",id).order("id DESC") }
+
+    def next
+      Manga.next(self.id).first
+    end
+
+    def prev
+      Manga.prev(self.id).first
+    end
 end
